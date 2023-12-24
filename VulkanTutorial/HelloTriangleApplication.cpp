fd80da68448a19b111869113ce138a1bff16e9b9
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-24 15:24:15 +0900

    ウインドウを表示するコードを書いた

--- VulkanTutorial/HelloTriangleApplication.cpp
+++ VulkanTutorial/HelloTriangleApplication.cpp
@@ -1,13 +1,32 @@
 #include "HelloTriangleApplication.h"
 
 void HelloTriangleApplication::run() {
+	initWindow();
 	initVulkan();
 	mainLoop();
 	cleanup();
 }
 
+void HelloTriangleApplication::initWindow() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
 void HelloTriangleApplication::initVulkan() {}
 
-void HelloTriangleApplication::mainLoop() {}
+void HelloTriangleApplication::mainLoop() {
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+}
 
-void HelloTriangleApplication::cleanup() {}
+void HelloTriangleApplication::cleanup() {
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+}
--- VulkanTutorial/HelloTriangleApplication.h
+++ VulkanTutorial/HelloTriangleApplication.h
@@ -1,12 +1,24 @@
 #pragma once
+
+#include <GLFW/glfw3.h>
+
+#include <cstdint>
+
 class HelloTriangleApplication
 {
 public:
 	void run();
 
 private:
+	void initWindow();
 	void initVulkan();
 	void mainLoop();
 	void cleanup();
+
+private:
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
+
+	GLFWwindow* window = nullptr;
 };
 
