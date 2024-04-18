c204a90e774c34fdc4e91acfb4222383ae9aca17
Taku Yamamoto <jts0ymt@outlook.jp>
2024-04-18 21:19:45 +0900

    パイプラインを作る関数を宣言した

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -118,6 +118,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createGraphicsPipeline();
     }
 
     void mainLoop() {
@@ -403,6 +404,10 @@ private:
                 throw std::runtime_error("failed to create image views!");
             }
         }
+    }
+
+    void createGraphicsPipeline() {
+
     }
 
     VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
